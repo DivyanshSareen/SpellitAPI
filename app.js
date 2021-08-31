3b0bb409ecaa49b0657b017dfde064b3127cc16f
DivyanshSareen <sareendivyansh@gmail.com>
2021-08-31 19:46:47 +0530

    added cors to the project

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const {spell} = require('./generate.js');
 const app = express();
+var cors = require('cors')
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
 
 var midw = function(req,res,next) {
-  res.send(spell.suggest(req.params.x));
+  req.dat = ({"ans" : spell.suggest(req.params.x)});
   next();
 }
 
@@ -22,6 +24,7 @@ app.get('/api/spell/:x', midw,(req,res)=>{
     // var wor = spell.suggest(req.params.x);
     // res.send(wor);
     // res.send(req.wid);
+    res.send(req.dat);
 });
 
 const port = process.env.PORT || 3000;
